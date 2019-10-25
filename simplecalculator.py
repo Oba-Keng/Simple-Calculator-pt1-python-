81bd8b1e24db9cd2299f8657f96778248f7fd7f1
Your Name <you@example.com>
2019-10-25 13:59:08 +0200

    1st commit

--- simplecalculator.py
+++ simplecalculator.py
@@ -1,12 +1,16 @@
 #add function which returns the sum of two numbers
-def add(x,y):
-    result = x + y
-    return result
+def add(numbers):
+    for num in numbers:
+        num += numbers[0]
+    return num
+
 
 #multiply function which returns sum of multiples
-def multiply(a,b):
-    sum = a * b
-    return sum
+def multiply(numbers):
+    for num in numbers:
+        num *= numbers[0]
+        continue 
+    return num
         
             
 
--- simplecalculator_test.py
+++ simplecalculator_test.py
@@ -6,12 +6,20 @@ class TestSimpleCalculator(unittest.TestCase):
 
     def test_add(self):
         
-        self.assertEqual(simplecalculator.add(15,15),30)
-        self.assertEqual(simplecalculator.add(45,60),105)
-        self.assertEqual(simplecalculator.add(75,22),97)
+        self.assertEqual(simplecalculator.add([14,16]),30)
+        self.assertEqual(simplecalculator.add([45,60]),105)
+        self.assertEqual(simplecalculator.add([75,22]),97)
+
+        # self.assertEqual(simplecalculator.add([14,16,22]),52)
+        # self.assertEqual(simplecalculator.add([45,60,45,19]),169)
+        # self.assertEqual(simplecalculator.add([75,22,53,77]),227)
 
     def test_multiply(self):
-        
-        self.assertEqual(simplecalculator.multiply(5,10),50)
-        self.assertEqual(simplecalculator.multiply(7,5),35)
-        self.assertEqual(simplecalculator.multiply(9,9),81)
+         
+        self.assertEqual(simplecalculator.multiply([5,10]),50)
+        self.assertEqual(simplecalculator.multiply([7,5]),35)
+        self.assertEqual(simplecalculator.multiply([9,9]),81)
+
+        # self.assertEqual(simplecalculator.multiply([5,10,2]),100)
+        # self.assertEqual(simplecalculator.multiply([7,5,10,9]),3150)
+        # self.assertEqual(simplecalculator.multiply([10,7,5,9,12,6]),226800)
