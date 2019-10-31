74325b8813bb7ea3f6f9b8fb9c71326ebd965ae9
Your Name <you@example.com>
2019-10-31 12:20:06 +0200

    2nd commit

---
+++ spec/test_simplecalculator.py
@@ -0,0 +1,25 @@
+import sys
+sys.path.append('../src')
+
+import simplecalculator
+
+
+
+def test_add():
+        assert simplecalculator.add(14,16) == 30
+        assert simplecalculator.add(45,60) == 105
+        assert simplecalculator.add(75,22) == 97
+
+        assert simplecalculator.add(14,16,22) == 52
+        assert simplecalculator.add(45,60,45,19) == 169
+        assert simplecalculator.add(75,22,53,77) == 227
+
+def test_multiply():
+        
+        assert simplecalculator.multiply(5,10) == 50
+        assert simplecalculator.multiply(7,5) == 35
+        assert simplecalculator.multiply(9,9) == 81
+
+        assert simplecalculator.multiply(5,10,2) == 100 
+        assert simplecalculator.multiply(7,5,10,9) == 3150
+        assert simplecalculator.multiply(10,7,5,9,12,6) == 226800
